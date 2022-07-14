1e39e2af1898ae39cc97f56dcfccece79fab8b5c
Cheolhwi <lizhehui@student.usm.my>
2022-07-14 22:46:30 +0800

    add function 1

--- func1.py
+++ func1.py
@@ -5,3 +5,12 @@ import networkx as nx
 # check if the graph is strongly connected
 def is_strongly_connected(graph):
     return nx.is_strongly_connected(graph)
+
+# generate random edges between random vertex until the graph is strongly connected
+def generate_strongly_connected_graph(graph):
+    while not is_strongly_connected(graph):
+        a = random.choice(init.vertex)
+        b = random.choice(init.vertex)
+        if (a != b):
+            graph.add_edge(a, b)
+    return graph
