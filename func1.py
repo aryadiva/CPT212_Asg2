befd35df73f110ce3ad6d5ca27287441fb6784ec
Cheolhwi <lizhehui@student.usm.my>
2022-07-14 23:48:53 +0800

    edit function1

--- func1.py
+++ func1.py
@@ -9,9 +9,18 @@ def is_strongly_connected(graph):
 
 # generate random edges between random vertex until the graph is strongly connected
 def generate_strongly_connected_graph(graph):
+    # remove all edges
+    graph.remove_edge('BA', 'VA')
+    graph.remove_edge('VA', 'PO')
+    graph.remove_edge('PO', 'SO')
+    graph.remove_edge('SO', 'VA')
+    graph.remove_edge('DU', 'BA')
     while not is_strongly_connected(graph):
         a = random.choice(init.vertex)
         b = random.choice(init.vertex)
+        # if already have an edge between two vertices, then skip
+        if graph.has_edge(a, b) and not graph.has_edge(b, a):
+            continue
         if a != b:
-            graph.add_edge(a, b)
+            graph.add_edge(a, b, weight=init.weight_dicts[a + b])
     return graph
