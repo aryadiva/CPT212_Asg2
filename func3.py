05812441a62c83686c0053fbbaa3a73f7506541e
AnasAnaqi <anasanaqi@student.usm.my>
2022-07-15 19:13:40 +0800

    Changed function 3 so that it checks for the path first then randomly creates path

--- func3.py
+++ func3.py
@@ -14,15 +14,13 @@ dist = init.weight_dicts
 def shortest_path(graph, start, end):
     # if there is no edge between two vertices, random generate until the path is found
     if not nx.has_path(graph, start, end):
-        i=0
-        while (i<40):
+        while (nx.has_path(graph, start, end)==False):
             a = random.choice(init.vertex)
             b = random.choice(init.vertex)
             if graph.has_edge(a, b) and not graph.has_edge(b, a):
                 continue
             if a != b:
                 graph.add_edge(a, b, weight=init.weight_dicts[a + b])
-            i += 1
         return nx.shortest_path(graph, start, end)
     else:
         return nx.shortest_path(graph, start, end)
